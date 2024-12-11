c99c0ded193ec73c91fe966f53c40f7557640e5c
Md. Anwar Hossain <anwarhossain7736@gmail.com>
2024-12-11 15:26:22 +0600

    Something changes

--- app/components/User.jsx
+++ app/components/User.jsx
@@ -1,12 +1,18 @@
 const User = ({ user }) => {
     return (
-        <div className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md" onClick={()=> alert(user?.id)}>
+        <div className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
             <div className="w-12 h-12 bg-gray-300 rounded-full mr-3">
                 <img src="https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato" alt="User Avatar" className="w-12 h-12 rounded-full" />
             </div>
             <div className="flex-1">
-                <h2 className="text-lg font-semibold">{user?.name}</h2>
-                <p className="text-gray-600">{user?.message}</p>
+                <div className="flex">
+                    <div className="font-semibold cursor-pointer">
+                        <span className="ml-1 text-md">{user?.name}</span>
+                        <sup className="p-1 bg-green-500 ml-1 rounded" style={{ fontSize: '0px' }}></sup>
+                        <span className="px-1 bg-red-600 ml-1 rounded text-white text-xs">20</span>
+                    </div>
+                </div>
+                <p className="text-gray-600 text-xs">{user?.message}</p>
             </div>
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 "use client";
 import Messages from './components/Messages';
 import Sidebar from './components/Sidebar'
+import { FaVideo } from "react-icons/fa";
+import { MdAddIcCall } from "react-icons/md";
 
 const Home = () => {
   const messages = [{ id: 1, message: 'Hello how are you?' }];
@@ -10,8 +12,19 @@ const Home = () => {
       <div className="flex h-screen overflow-hidden w-full ">
         <Sidebar users={users} />
         <div className="w-3/4">
-          <header className="bg-white p-4 text-gray-700">
-            <h1 className="text-2xl font-semibold">Alice</h1>
+          <header className="bg-white p-4 text-gray-700 flex justify-between">
+            <div className="text-xl font-semibold cursor-pointer">
+              <span className="ml-1">John Doe</span> 
+              <sup className="p-1 bg-green-500 ml-1 rounded" style={{ fontSize: '0px' }}></sup> 
+              </div>
+            <div className="flex justify-end px-2">
+              <div className="px-3 cursor-pointer">
+                <MdAddIcCall title="voice call" />
+              </div>
+              <div className="px-3 cursor-pointer">
+                <FaVideo title="video call" />
+              </div>
+            </div>
           </header>
           <Messages messages={messages} />
 
