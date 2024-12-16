c546501d9a70d8bc886ce50537498dd304ceec2b
anwar7736 <anwarhossain7736@gmail.com>
2024-12-17 00:58:12 +0800

    Something changes

--- app/components/Messages.jsx
+++ app/components/Messages.jsx
@@ -9,7 +9,7 @@ const Messages = ({ messages }) => {
                         {
                             message?.sender[0]?._id == auth()?._id
                                 ?
-                                (<div>
+                                (<div className="mb-3">
                                     <div className="flex justify-end mb-1 cursor-pointer">
                                         <div className="flex max-w-96 bg-indigo-500 text-white rounded-lg p-3 gap-3">
                                             <p>{message?.message}</p>
@@ -24,7 +24,9 @@ const Messages = ({ messages }) => {
                                 </div>)
                                 :
                                 (
-                                    <div>
+                                    <div className="mb-3">
+                                        <div align="left" className="text-xs text-gray-500 ml-10">{message?.sender[0]?.name}
+                                        </div>
                                         <div className="flex mb-1 cursor-pointer">
                                             <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
                                                 <img src="https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato" alt="User Avatar" className="w-8 h-8 rounded-full" />
